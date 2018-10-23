67a2d03195ff12a01df827adde432fad54247a60
andrei1058 <andrew.dascalu@gmail.com>
2018-10-23 13:56:18 +0200

    fixed

--- src/main/java/com/andrei1058/bedwars/healpool/HealPoolTask.java
+++ src/main/java/com/andrei1058/bedwars/healpool/HealPoolTask.java
@@ -20,7 +20,6 @@ public class HealPoolTask extends BukkitRunnable {
     private int maxX, minX;
     private int maxY, minY;
     private int maxZ, minZ;
-    private int radius;
     private Arena arena;
     private Random r = new Random();
     private Block b;
@@ -29,7 +28,7 @@ public class HealPoolTask extends BukkitRunnable {
 
     public HealPoolTask(BedWarsTeam bwt) {
         this.bwt = bwt;
-        this.radius = bwt.getArena().getCm().getInt(ConfigPath.ARENA_ISLAND_RADIUS);
+        int radius = bwt.getArena().getCm().getInt(ConfigPath.ARENA_ISLAND_RADIUS);
         this.maxX = Math.max(bwt.getSpawn().clone().add(radius, 0, 0).getBlockX(), bwt.getSpawn().clone().subtract(radius, 0, 0).getBlockX());
         this.minX = Math.min(bwt.getSpawn().clone().add(radius, 0, 0).getBlockX(), bwt.getSpawn().clone().subtract(radius, 0, 0).getBlockX());
         this.maxY = Math.max(bwt.getSpawn().clone().add(0, radius, 0).getBlockY(), bwt.getSpawn().clone().subtract(0, radius, 0).getBlockY());
@@ -37,16 +36,26 @@ public class HealPoolTask extends BukkitRunnable {
         this.maxZ = Math.max(bwt.getSpawn().clone().add(0, 0, radius).getBlockZ(), bwt.getSpawn().clone().subtract(0, 0, radius).getBlockZ());
         this.minZ = Math.min(bwt.getSpawn().clone().add(0, 0, radius).getBlockZ(), bwt.getSpawn().clone().subtract(0, 0, radius).getBlockZ());
         this.arena = bwt.getArena();
-        this.runTaskTimer(Main.plugin, 0, 1L);
+        this.runTaskTimer(Main.plugin, 0, 40L);
         healPoolTasks.add(this);
     }
 
     @Override
     public void run() {
-        b = new Location(bwt.getSpawn().getWorld(), r.nextInt(minX)+radius, r.nextInt(minY)+radius, r.nextInt(minZ)+radius).getBlock();
-        if (b.getType() != Material.AIR) return;
-        for (Player p : bwt.getMembers()) {
-            Main.getVersionSupport().playEffect(p, b.getLocation());
+        Block b;
+        for (int x = minX; x < maxX; x++) {
+            for (int y = minY; y < maxY; y++) {
+                for (int z = minZ; z < maxZ; z++) {
+                    b = new Location(bwt.getSpawn().getWorld(), x, y, z).getBlock();
+                    if (b.getType() != Material.AIR) continue;
+                    int chance = r.nextInt(2);
+                    if (chance == 0) {
+                        for (Player p : bwt.getMembers()) {
+                            Main.getVersionSupport().playEffect(p, b.getLocation());
+                        }
+                    }
+                }
+            }
         }
     }
 
